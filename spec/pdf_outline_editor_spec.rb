f8f3e8b1ed5a1ca3c821a59d92d583f24645dfa4
healthypackrat <healthypackrat@gmail.com>
2019-02-28 00:02:18 +0900

    Configure RSpec

--- spec/pdf_outline_editor_spec.rb
+++ spec/pdf_outline_editor_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe PdfOutlineEditor do
   it "has a version number" do
     expect(PdfOutlineEditor::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
